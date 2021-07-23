16fce4be1111368e97ea648e237c1b443ebacd0f
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-07-23 22:59:03 +0300

    creates user registration function

--- mtaa/views.py
+++ mtaa/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+
+from .forms import CreateUserForm
 
 # Create your views here.
 
@@ -6,3 +12,21 @@ def index(request):
     title = "MtaaInfo Application"
 
     return render(request, 'index.html',{"title":title})
+
+def register(request):
+	if request.user.is_authenticated:
+		return redirect('index')
+	else:
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request, 'Account was created for ' + user)
+
+				return redirect('login')
+			
+
+		context = {'form':form}
+		return render(request, 'registration/register.html', context)

a267d486bfed921cfdd602c09d26c94bddc59f87
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-07-23 23:40:30 +0300

    creates login template

--- mtaa/urls.py
+++ mtaa/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns=[
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('register/', views.register, name="register"),
+	path('login/', views.loginUser, name="login"), 
+    path('logout/',views.logoutUser,name="logout") ,
 ]
